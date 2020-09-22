81ecc1207d6b2602d683503edc3d43af9bb82445
chandu9515 <chandu.kodavaluru1811@gmail.com>
2020-09-22 15:56:49 +0530

    Added Project Data

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    img = models.ImageField(upload_to='blog/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+
+    def __str__(self):
+        return self.title
+
+    
